ef663d964923e952a9a080c5e46d019dd995637f
Antons Dolgopolovs <antdolg@gmail.com>
2023-08-17 22:28:04 +0300

    Homework lesson2 task 1 and 2

---
+++ src/main/java/students/julija_pilenkova/homework/lesson2/level1_task1.java
@@ -0,0 +1,26 @@
+package students.julija_pilenkova.homework.lesson2;
+
+import java.util.Scanner;
+
+public class level1_task1 {
+
+    public static void main (String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число:");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Введите второе число:");
+        int num2 = scanner.nextInt();
+
+        int sum1 = num1 + num2;
+        int sum2 = num1 - num2;
+        int sum3 = num1 * num2;
+        int sum4 = num1 / num2;
+
+        System.out.println("Результат сложения:" + sum1);
+        System.out.println("Результат вычитания:" + sum2);
+        System.out.println("Результат умножения:" + sum3);
+        System.out.println("Результат деления:" + sum4);
+    }
+}
---
+++ src/main/java/students/julija_pilenkova/homework/lesson2/level1_task2.java
@@ -0,0 +1,28 @@
+package students.julija_pilenkova.homework.lesson2;
+
+import java.util.Scanner;
+
+public class level1_task2 {
+
+    public static void main (String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("First number:");
+        double num1 = scanner.nextFloat();
+
+        System.out.println("Second number:");
+        double num2 = scanner.nextFloat();
+
+        double sum1 = num1 + num2;
+        double sum2 = num1 - num2;
+        double sum3 = num1 * num2;
+        double sum4 = num1 / num2;
+
+
+        System.out.println("Sum of num 1 and num2 is " + sum1 );
+        System.out.println("Substraction of num 1 and num2 is " + sum2 );
+        System.out.println("Multiplications of num 1 and num2 is " + sum3 );
+        System.out.println("Devision of num 1 and num2 is " + sum4 );
+    }
+}
