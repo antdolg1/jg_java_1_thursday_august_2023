cc5d11ef8af1b3fcdbeefebbf959903a17cff20e
denis-vil <denis-osipenko@mail.ru>
2023-08-18 20:34:05 +0300

    renamed

---
+++ src/main/java/students/denis_asipenka/lesson_2/level_7/Task_32.java
@@ -0,0 +1,15 @@
+package students.denis_asipenka.lesson_2.level_7;
+
+public class Task_32 {
+    public static void main(String[] args) {
+        int a=5; // Присваиваем переменной а значение 5
+        int b=9;
+        int c=a+b;
+        System.out.print("Цифра "+a+" + цифра "+b+" = "+c);
+
+        /*
+        Программа складывает два целых числа и выводит результат на экран
+
+         */
+    }
+}
