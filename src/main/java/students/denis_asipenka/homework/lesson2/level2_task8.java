238aaec72cc00ef2502e8c9988795adf05eb28b1
denis-vil <denis-osipenko@mail.ru>
2023-08-18 13:19:33 +0300

    name input and output task

---
+++ src/main/java/students/denis_asipenka/homework/lesson2/level2_task8.java
@@ -0,0 +1,15 @@
+package students.denis_asipenka.homework.lesson2;
+import java.util.Scanner;
+public class level2_task8 {
+    public static void main(String[] args) {
+        System.out.println("Введите имя:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+
+
+
+
+    }
+}
