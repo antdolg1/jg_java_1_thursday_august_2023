e90be4ce16a22c082bd7fbc55d2d7afc41bd56dc
denis-vil <denis-osipenko@mail.ru>
2023-08-18 13:12:01 +0300

    level2_task3 and level2_task4

---
+++ src/main/java/students/denis_asipenka/homework/lesson2/level2_task3.java
@@ -0,0 +1,12 @@
+package students.denis_asipenka.homework.lesson2;
+
+public class level2_task3 {
+    /*
+
+    public class Book{
+    }
+             добавил public
+             добавил закрывающую скобку
+    */
+
+}
---
+++ src/main/java/students/denis_asipenka/homework/lesson2/level2_task4.java
@@ -0,0 +1,12 @@
+package students.denis_asipenka.homework.lesson2;
+
+public class level2_task4 {
+   /*
+
+    public class Phone{
+     }
+
+     добавил открывающую скобку и public
+    */
+
+}
