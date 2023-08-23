c47a608bcff0a6a76256ea7af4c803dc29bb5c43
Nik737 <nikolaj.antonov@gmail.com>
2023-08-23 23:12:23 +0300

    homework level_1, level_3

---
+++ src/main/java/students/nikolajs_antonovs/lesson_2/level_3/TestData.java
@@ -0,0 +1,22 @@
+package students.nikolajs_antonovs.lesson_2.level_3;
+
+import java.util.Scanner;
+
+public class TestData {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Test Data: ");
+        System.out.println("Input a number: ");
+
+        int number = scanner.nextInt();
+        System.out.println("Expected Output: ");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int result = number * i;
+            System.out.println(number + "x" + "=" + result);
+        }
+    }
+}
