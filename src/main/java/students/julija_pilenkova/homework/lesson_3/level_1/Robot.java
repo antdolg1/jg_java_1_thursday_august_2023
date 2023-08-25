9dc95e3ed98e7a497d2edd01fef65d338060686d
JulliaPi <julijapilenkova@gmail.com>
2023-08-26 00:29:47 +0300

    Lesson 3 homework

---
+++ src/main/java/students/julija_pilenkova/homework/lesson_3/level_1/Robot.java
@@ -0,0 +1,24 @@
+package students.julija_pilenkova.homework.lesson_3.level_1;
+
+public class Robot {
+
+    public String name;
+
+    Robot (String robotName) {
+        this.name = robotName;
+
+    }
+
+
+    public void sayHello() { System.out.println("Hello!");}
+    public void sayYourName() { System.out.println("My name is " + name);
+    }
+
+
+
+    public Robot() {
+
+
+    }
+
+}
---
+++ src/main/java/students/julija_pilenkova/homework/lesson_3/level_4/Book.java
@@ -0,0 +1,27 @@
+package students.julija_pilenkova.homework.lesson_3.level_4;
+
+public class Book {
+
+    String title;
+
+    Book(String bookTitle) {
+        this.title = bookTitle; //ispravila na bookTitle
+    }
+
+    String getTitle() {
+        return this.title;
+    }
+
+}
+
+class BookDemo {
+
+    public static void main(String[] args) {
+        Book myBook = new Book("Principles");
+        String bookTitle = myBook.getTitle();
+        System.out.println("Book title = " + bookTitle);
+    }
+
+
+
+}
