5e4e553838d021a204711f48228936878d66584a
Aleksandrs Kunicins <tiv51tiv51@gmail.com>
2023-08-25 16:10:21 +0300

    task 2 in lesson 3 - aleksandrs

---
+++ src/main/java/students/aleksandrs_kunicins/homework/lesson_3/level_1/Task_2.java
@@ -0,0 +1,7 @@
+package students.aleksandrs_kunicins.homework.lesson_3.level_1;
+
+public class Task_2 {       // В нашей папке
+    public class Robot{     // В классе Robot
+        public Robot(){}    // Создаём функцию, которая ничего не делает
+    }
+}
