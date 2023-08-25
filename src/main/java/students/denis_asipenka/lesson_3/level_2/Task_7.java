1d48f61a859244b8965c2bcf20237efa75940cfd
denis-vil <denis-osipenko@mail.ru>
2023-08-26 00:19:52 +0300

    del

--- src/main/java/students/denis_asipenka/lesson_3/level_2/Task_7.java
+++
@@ -1,8 +0,0 @@
-package students.denis_asipenka.lesson_3.level_2;
-
-public class Task_7 {
-    public static void main(String[] args) {
-        String myName = "Denis";
-        System.out.println(myName);
-    }
-}
