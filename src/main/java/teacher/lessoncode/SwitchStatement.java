a49d9725c4ab2e0f1887fccf6193aac7842f36ec
Antons Dolgopolovs <antdolg@gmail.com>
2023-08-31 21:54:53 +0300

    Add lesson_4 lessoncode, materials and homework

---
+++ src/main/java/teacher/lessoncode/SwitchStatement.java
@@ -0,0 +1,68 @@
+package teacher.lessoncode;
+
+import java.util.Scanner;
+
+public class SwitchStatement {
+
+    public static void main(String[] args) {
+
+//        int dayOfTheWeek = 1;
+//
+//        switch (dayOfTheWeek) {
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            case 4:
+//                System.out.println("Thursday");
+//                break;
+//            case 5:
+//                System.out.println("Friday...finally!");
+//                break;
+//            default:
+//                System.out.println("Weeeekeeeeeends!");
+//        }
+
+        System.out.println("Please input number from 1 to 6:");
+
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber = scanner.nextInt();
+
+//        switch (inputNumber) {
+//            case 1:
+//            case 2:
+//                System.out.println("Junior");
+//                break;
+//            case 3:
+//                System.out.println("Middle");
+//                break;
+//            case 4:
+//                System.out.println("Senior");
+//                break;
+//            case 5:
+//                System.out.println("Solution architect");
+//                break;
+//            case 6:
+//                System.out.println("CTO");
+//                break;
+//            default:
+//                System.out.println("Wrong number entered!");
+//        }
+
+        //enhanced switch statement, available since java 13
+        switch (inputNumber) {
+            case 1, 2 -> System.out.println("Junior");
+            case 3 -> System.out.println("Middle");
+            case 4 -> System.out.println("Senior");
+            case 5 -> System.out.println("Solution architect");
+            case 6 -> System.out.println("CTO");
+            default -> System.out.println("Wrong number entered!");
+        }
+
+    }
+}
---
+++ src/main/java/teacher/lessoncode/TernaryOperator.java
@@ -0,0 +1,27 @@
+package teacher.lessoncode;
+
+public class TernaryOperator {
+
+    public static void main(String[] args) {
+
+        int number = 5;
+
+        if (number % 2 == 0) {
+            System.out.println("Even number");
+        } else {
+            System.out.println("Odd number");
+        }
+
+        //ternary operator way
+        //синтаксис тернарного оператора
+        //условие ? значение_если_истина : значение_если_ложь;
+        String result = (number % 2 == 0) ? "Чётное" : "Нечётное";
+
+        System.out.println(result);
+
+        boolean isSunny = false;
+        String currentWeatherMessage = isSunny ? "Отличная погодка" : "Надоел этот дождь";
+        System.out.println(currentWeatherMessage);
+    }
+
+}
