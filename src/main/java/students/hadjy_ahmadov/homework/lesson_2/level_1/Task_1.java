8ab5e96ec7e69d69214a5d099462c92f06e28f2b
Pixel2Y <iscintillat@gmail.com>
2023-08-31 18:43:02 +0300

    homework

---
+++ src/main/java/students/hadjy_ahmadov/homework/lesson_2/level_1/Task_1.java
@@ -0,0 +1,34 @@
+package students.hadjy_ahmadov.homework.lesson_2.level_1;
+
+import java.util.Scanner;
+
+public class Task_1 {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //1st number request
+        System.out.println("Enter 1st number:");
+        int num1 = scanner.nextInt();
+
+        //2nd number request
+        System.out.println("Enter 2nd number:");
+        int num2 = scanner.nextInt();
+
+        //Operations calculation and output of results
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        double quotinent = (double) num1 / num2; // Division as double for precision
+
+        System.out.println("Results:");
+        System.out.println("Addition:" +sum);
+        System.out.println("Subtraction:" +difference);
+        System.out.println("Multiply:" +product);
+        System.out.println("Division:" +quotinent);
+
+        //Scanner closing
+        scanner.close();
+
+
+    }
+}
---
+++ src/main/java/students/hadjy_ahmadov/homework/lesson_2/level_2/Task_4.java
@@ -0,0 +1,6 @@
+package students.hadjy_ahmadov.homework.lesson_2.level_2;
+
+public class Task_4 {
+    class Phone {
+    }
+}
