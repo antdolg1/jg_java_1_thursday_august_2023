5ca6e9b86fe5b71bbcc49eddd0e580745eb8cf2c
krist <konovalchukkristina8@gmail.com>
2023-09-04 19:40:57 +0300

    test commit

---
+++ src/main/java/students/kristina_konovalchuk/lesson2/level3/Task_9.java
@@ -0,0 +1,40 @@
+package students.kristina_konovalchuk.lesson2.level3;
+
+import java.util.Scanner;
+
+public class Task_9 {
+
+   public static void main(String[] args){
+
+       Scanner scanner = new Scanner(System.in);
+
+       System.out.println("Введите чило первое число : ");
+       int num1 = scanner.nextInt();
+       System.out.println("Введите второе число : ");
+       int num2 = scanner.nextInt();
+       if (num1+num2 >10)  {
+           System.out.println("Число должно быть меньше 10 ! ");
+
+
+       }
+       else {
+           int result = num1 * num2 ;
+           System.out.println("Результат :" +  result);
+       }
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
