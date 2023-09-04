f35f18f079d3a73ae47d7223658ac9f3e52f792a
Aleksandrs Kunicins <tiv51tiv51@gmail.com>
2023-09-04 23:29:08 +0300

    homework update

---
+++ src/main/java/students/aleksandrs_kunicins/homework/lesson_4/level_4/Task_11.java
@@ -0,0 +1,17 @@
+package students.aleksandrs_kunicins.homework.lesson_4.level_4;
+
+class Calculator{
+    public int sum(int firstNumber, int secondNumber){
+        return firstNumber + secondNumber;
+    }
+    public int diff(int firstNumber, int secondNumber){
+        return firstNumber - secondNumber;
+    }
+        public int mult(int firstNumber, int secondNumber){
+        return firstNumber * secondNumber;
+    }
+        public int div(int firstNumber, int secondNumber){
+        return firstNumber / secondNumber;
+    }
+
+}
