5414ddb31f44db6e8c833113b357aca5b5e3ac81
krist <konovalchukkristina8@gmail.com>
2023-09-04 18:45:52 +0300

    test commit

---
+++ src/main/java/students/kristina_konovalchuk/lesson2/level2/Task_7.java
@@ -0,0 +1,12 @@
+package students.kristina_konovalchuk.lesson2.level2;
+
+public class Task_7 {
+
+    class LoginService{
+
+        public static void main(String[] args){
+
+            System.out.println("You can login!");
+        }
+    }
+}
---
+++ src/main/java/students/kristina_konovalchuk/lesson2/level2/Task_8.java
@@ -0,0 +1,14 @@
+package students.kristina_konovalchuk.lesson2.level2;
+
+import java.util.Scanner;
+
+public class Task_8 {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите им'я пользователя: ");
+        String username = scanner.nextLine();
+        System.out.println("Добро пожаловать!");
+    }
+}
