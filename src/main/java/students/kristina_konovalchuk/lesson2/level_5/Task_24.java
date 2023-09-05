188e76cd60de9409d893385bdb0de6e909ae4ed5
krist <konovalchukkristina8@gmail.com>
2023-09-05 18:11:04 +0300

    test commit

---
+++ src/main/java/students/kristina_konovalchuk/lesson2/level_5/Task_24.java
@@ -0,0 +1,9 @@
+package students.kristina_konovalchuk.lesson2.level_5;
+
+public class Task_24 {
+
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}
