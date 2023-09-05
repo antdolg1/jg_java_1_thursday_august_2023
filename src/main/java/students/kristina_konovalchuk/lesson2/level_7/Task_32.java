75f364e9639ce57c9ed998a3b35d883d9f42db80
krist <konovalchukkristina8@gmail.com>
2023-09-05 18:24:48 +0300

    test commit

---
+++ src/main/java/students/kristina_konovalchuk/lesson2/level_7/Task_32.java
@@ -0,0 +1,19 @@
+package students.kristina_konovalchuk.lesson2.level_7;
+
+import java.util.Scanner;
+
+public class Task_32 {
+
+    public static void main(String [] args){
+
+       /* Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+
+        */
+
+       // System.out.println("Hello!");
+
+
+
+    }
+}
