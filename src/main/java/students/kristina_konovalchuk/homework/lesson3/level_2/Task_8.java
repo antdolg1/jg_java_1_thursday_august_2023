33ab54fd0878d63d0d29f0238e8b9e560af7ddb7
krist <konovalchukkristina8@gmail.com>
2023-09-09 22:34:53 +0300

    test commit

---
+++ src/main/java/students/kristina_konovalchuk/homework/lesson3/level_2/Task_8.java
@@ -0,0 +1,14 @@
+package students.kristina_konovalchuk.homework.lesson3.level_2;
+
+public class Task_8 {
+    public static void main(String []args){
+
+        String userName = "Kristina";
+        String greeting = "Hi" + userName + "!" ;
+
+        System.out.println(greeting + userName);
+
+
+    }
+
+}
