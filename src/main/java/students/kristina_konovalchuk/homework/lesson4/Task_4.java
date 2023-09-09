ec909a15d2fe33af00cb42485a16da46d7bcdc25
krist <konovalchukkristina8@gmail.com>
2023-09-09 23:21:18 +0300

    test commit

---
+++ src/main/java/students/kristina_konovalchuk/homework/lesson4/Task_4.java
@@ -0,0 +1,20 @@
+package students.kristina_konovalchuk.homework.lesson4;
+
+import java.util.Scanner;
+
+public class Task_4 {
+    public static void main(String []args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое число: ");
+
+        int number = scanner.nextInt();
+
+        if (number % 2==0){
+            System.out.println("Число четное ");
+        }
+        else {
+            System.out.println("Чило нечетное ");
+        }
+    }
+}
