de4f1d928f7e088b08c4a9f69ccc3302245b4a2d
krist <konovalchukkristina8@gmail.com>
2023-09-09 22:34:28 +0300

    test commit

---
+++ src/main/java/students/kristina_konovalchuk/homework/lesson3/level_2/Task_9.java
@@ -0,0 +1,15 @@
+package students.kristina_konovalchuk.homework.lesson3.level_2;
+
+import java.util.Scanner;
+
+public class Task_9 {
+    public static void main(String []args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Как вас зовут?" );
+
+        String name = scanner.nextLine();
+        System.out.println("Hello!" + name);
+    }
+
+}
