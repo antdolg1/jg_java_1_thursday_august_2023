582231f61a4beebc54a75268380949dbf2219ec9
NikitaTrunkov <trunkov1990@gmail.com>
2023-09-19 10:59:20 +0300

    lesson 2 added

---
+++ src/main/java/students/nikita_trunkov/homework/lesson_2/level_6/Task_23.java
@@ -0,0 +1,9 @@
+package students.nikita_trunkov.homework.lesson_2.level_6;
+
+public class Task_23 {
+   static class AttentionPlease {
+        public static void main(String[] args) {
+            System.out.println("Code outside the method will not compile!");
+        }
+    }
+}
---
+++ src/main/java/students/nikita_trunkov/homework/lesson_2/level_6/Task_24.java
@@ -0,0 +1,10 @@
+package students.nikita_trunkov.homework.lesson_2.level_6;
+
+public class Task_24 {
+    static class NoSleepNoFocus {
+        public static void main(String[] args) {
+            System.out.println("I wanna coffee");
+            System.out.println("So bad");
+        }
+    }
+}
