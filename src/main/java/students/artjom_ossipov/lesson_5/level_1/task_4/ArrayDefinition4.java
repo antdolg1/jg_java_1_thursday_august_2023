72af9b46572e4ed2f870c79140bf8726fca7f5ce
ArtjomOssipov <ossipovartjom@gmail.com>
2023-09-26 12:39:48 +0300

    Lesson 5, homework

---
+++ src/main/java/students/artjom_ossipov/lesson_5/level_1/task_4/ArrayDefinition4.java
@@ -0,0 +1,7 @@
+package students.artjom_ossipov.lesson_5.level_1.task_4;
+
+public class ArrayDefinition4 {
+    public static void main(String[] args) {
+        int[] numbers = new int[] {1, 2, 3};
+    }
+}
---
+++ src/main/java/students/artjom_ossipov/lesson_5/level_2/task_14/Task14.java
@@ -0,0 +1,21 @@
+package students.artjom_ossipov.lesson_5.level_2.task_14;
+
+import java.util.Random;
+
+public class Task14 {
+    public static void main(String[] args) {
+        Random r = new Random();
+
+        int[] numbers = new int[3];
+
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            int r_number = r.nextInt(1000);
+            numbers[i] = r_number;
+            System.out.println("Случайное число = " + r_number); // для проверки, какие числа складывает
+            sum += numbers[i];
+        }
+        System.out.println( "Сумма случайных чисел = " + sum/3); // разделил сумму на 3
+    }
+}
