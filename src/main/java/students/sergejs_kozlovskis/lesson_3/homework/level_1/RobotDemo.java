2d6ab3b7105fc3d0bf2e9f34ea733746afb3a863
SergeMetr <sergejskozlovskis@googlemail.com>
2023-09-30 16:04:42 +0300

    Homework commit

---
+++ src/main/java/students/sergejs_kozlovskis/lesson_3/homework/level_1/RobotDemo.java
@@ -0,0 +1,26 @@
+package students.sergejs_kozlovskis.lesson_3.homework.level_1;
+
+public class RobotDemo {
+
+    public static void main (String[] args)
+    {
+        //Task 3
+        Robot myRobot = new Robot();
+
+        //Task 4
+        myRobot.sayHello();
+
+        //Task 5
+        myRobot.sayYourName();
+
+        //Task 6
+
+        Robot myRobot1 = new Robot();
+        myRobot1.sayHello();
+        myRobot1.sayYourName();
+
+        Robot myRobot2 = new Robot();
+        myRobot2.sayHello();
+        myRobot2.sayYourName();
+    }
+}
---
+++ src/main/java/students/sergejs_kozlovskis/lesson_3/homework/level_2/Task_7.java
@@ -0,0 +1,9 @@
+package students.sergejs_kozlovskis.lesson_3.homework.level_2;
+
+public class Task_7 {
+    public static void main(String [] args){
+
+        String myName = "Sergejs";
+        System.out.println(myName);
+    }
+}
