a4fcb9950df6b84f7763b2b92b391761f643dfc6
anastassia <anastassia.lj@gmail.com>
2023-10-02 21:06:05 +0300

    Lesson 2 homework task 1

---
+++ src/main/java/students/anastassia_iljina/lesson_2/Calculator_Task_1.java
@@ -0,0 +1,28 @@
+package students.anastassia_iljina.lesson_2;
+
+import java.util.Scanner;
+
+public class Calculator_Task_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("Введите первое целое число: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.println("Введите второе целое число: ");
+        int secondNumber = scanner.nextInt();
+
+        int sum = firstNumber + secondNumber;
+        int subtraction = firstNumber - secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        int division = firstNumber / secondNumber;
+
+        System.out.println("Результат сложения: " + sum);
+        System.out.println("Результат вычитания: " + subtraction);
+        System.out.println("Результат умножения: " + multiplication);
+        System.out.println("Результат деления: " + division);
+
+
+
+    }
+}
