60bd84e1ddc38a4fedbaa2bc881cd2a0ec58ab47
ArtjomOssipov <ossipovartjom@gmail.com>
2023-10-06 13:23:16 +0300

    lesson 6, homework, ossipov artjom

---
+++ src/main/java/students/artjom_ossipov/lesson_6/level_1/Task_5/WhileDefinition5.java
@@ -0,0 +1,11 @@
+package students.artjom_ossipov.lesson_6.level_1.Task_5;
+
+public class WhileDefinition5 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
---
+++ src/main/java/students/artjom_ossipov/lesson_6/level_1/Task_6/NumberUtils.java
@@ -0,0 +1,9 @@
+package students.artjom_ossipov.lesson_6.level_1.Task_6;
+
+import java.util.Scanner;
+
+public class NumberUtils {
+    public boolean isEven (int number) {
+        return number % 2 == 0;
+    }
+}
