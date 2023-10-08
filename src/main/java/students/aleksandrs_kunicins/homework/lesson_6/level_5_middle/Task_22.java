c88fb9fc7b812af0855fc0b308725b51ba38c7ad
Aleksandrs Kunicins <tiv51tiv51@gmail.com>
2023-10-08 22:10:14 +0300

    finishing lesson 6

--- src/main/java/students/aleksandrs_kunicins/homework/lesson_6/level_5_middle/Task_22.java
+++
@@ -1,5 +0,0 @@
-package students.aleksandrs_kunicins.homework.lesson_6.level_5_middle;
-
-public class Task_22 {
-    
-}
---
+++ src/main/java/students/aleksandrs_kunicins/homework/lesson_6/level_6_middle/Task_28.java
@@ -0,0 +1,19 @@
+package students.aleksandrs_kunicins.homework.lesson_6.level_6_middle;
+
+class Move {
+    private int x;
+    private int y;
+
+    public Move(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
