20d9314500bfe0e49cd9356ecd7b8d0ce7f8167a
Antons Dolgopolovs <antdolg@gmail.com>
2023-10-12 22:15:15 +0300

    Added explanation to array problem

--- src/main/java/teacher/lesson_9/lessoncode/array_problem/SortSelect.java
+++ src/main/java/teacher/lesson_9/lessoncode/array_problem/SortSelect.java
@@ -15,3 +15,14 @@ public class SortSelect {
         }
     }
 }
+
+/**
+ * Sort the array in ascending order.
+ * The two largest numbers will now be at the end of the array, and the two smallest numbers will be at the beginning.
+ * Calculate the product of the two largest numbers.
+ * Calculate the product of the two smallest numbers.
+ * Compare the two products:
+ * If the product of the two smallest numbers is greater, it means they are both large negative numbers, and their product is the maximum.
+ * Otherwise, the product of the two largest numbers is the maximum.
+ * Return the pair of numbers with the maximum product.
+ */
