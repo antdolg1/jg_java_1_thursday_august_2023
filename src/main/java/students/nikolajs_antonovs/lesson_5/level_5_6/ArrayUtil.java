bb04c5468c171475b94f10c4141846fd526e859d
Nik737 <nikolaj.antonov@gmail.com>
2023-11-03 19:27:23 +0200

    homework lesson_5_fix

--- src/main/java/students/nikolajs_antonovs/lesson_5/level_5_6/ArrayUtil.java
+++ src/main/java/students/nikolajs_antonovs/lesson_5/level_5_6/ArrayUtil.java
@@ -17,6 +17,13 @@ class ArrayUtil {
         }
     }
 
+    public int[] createArray(int arrayLength) {
+        if (arrayLength <= 0) {
+            System.out.println("You want to create an impossible array!");
+        }
+        return new int[arrayLength];
+    }
+
     public int findMaxNumber(int[] numbers) {
         int maxNumber = numbers[0];
         for (int i = 0; i < numbers.length; i++) {
