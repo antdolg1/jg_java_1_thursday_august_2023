6e1121ccce042c614e8e6c5cafb72bd6a23bced4
Antons Dolgopolovs <antdolg@gmail.com>
2023-11-23 18:00:57 +0200

    Add lesson_15 lessoncode (sample project for refactoring)

---
+++ src/main/java/teacher/lesson_15/lessoncode/sample_code/OrderProcessor.java
@@ -0,0 +1,33 @@
+package teacher.lesson_15.lessoncode.sample_code;
+
+public class OrderProcessor {
+    private OrderDatabase db;
+    private PaymentService paymentService;
+    private NotificationService notificationService;
+
+    public OrderProcessor(OrderDatabase db, PaymentService paymentService, NotificationService notificationService) {
+        this.db = db;
+        this.paymentService = paymentService;
+        this.notificationService = notificationService;
+    }
+
+    public void processOrder(String orderId, String userToken) {
+        Order order = db.getOrderById(orderId);
+        if (order != null && order.getStatus() == OrderStatus.NEW) {
+            boolean paymentConfirmed = paymentService.processPayment(order.getTotalAmount(), userToken);
+            if (paymentConfirmed) {
+                order.setStatus(OrderStatus.PROCESSING);
+                db.updateOrder(order);
+                notificationService.sendNotification(order.getUser(), "Your order is being processed.");
+            } else {
+                notificationService.sendNotification(order.getUser(), "Payment failed. Please retry.");
+            }
+        } else if (order != null && order.getStatus() != OrderStatus.NEW) {
+            notificationService.sendNotification(order.getUser(), "Order cannot be processed.");
+        } else {
+            throw new IllegalArgumentException("Invalid order ID");
+        }
+    }
+
+    // Other methods...
+}
