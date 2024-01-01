02883ad15b3105fc33d5e7c0ae335c870de4a690
ArtjomOssipov <ossipovartjom@gmail.com>
2024-01-01 16:41:31 +0200

    Artjom Ossipov, lesson 13, level 1|2

---
+++ src/main/java/students/artjom_ossipov/lesson_13/level_1/TaxCalculator.java
@@ -0,0 +1,7 @@
+package students.artjom_ossipov.lesson_13.level_1;
+
+import java.math.BigDecimal;
+
+public interface TaxCalculator {
+    BigDecimal calculateTax(BigDecimal income);
+}
---
+++ src/test/java/artjom_ossipov/lesson_13/level_1/TaxCalculatorImplTest.java
@@ -0,0 +1,54 @@
+package artjom_ossipov.lesson_13.level_1;
+
+import org.junit.Before;
+import org.junit.Test;
+import students.artjom_ossipov.lesson_13.level_1.TaxCalculator;
+import students.artjom_ossipov.lesson_13.level_1.TaxCalculatorImpl;
+
+import java.math.BigDecimal;
+
+import static junit.framework.Assert.assertEquals;
+
+public class TaxCalculatorImplTest {
+    private TaxCalculator calculator;
+
+    @Before
+    public void initialize() {
+        calculator = new TaxCalculatorImpl();
+    }
+
+    @Test
+    public void calculateTaxWhenZeroIncome() {
+        BigDecimal income = BigDecimal.ZERO;
+        income = calculator.calculateTax(income);
+        assertEquals(income, new BigDecimal("0.00"));
+    }
+
+    @Test
+    public void shouldPay25PercentTaxWhenIncomeLessThen20000() {
+        BigDecimal income = new BigDecimal("10000");
+        income = calculator.calculateTax(income);
+        assertEquals(income, new BigDecimal("2500.00"));
+    }
+
+    @Test
+    public void shouldPay25PercentTaxWhenIncomeEqualTo20000() {
+        BigDecimal income = new BigDecimal("20000");
+        income = calculator.calculateTax(income);
+        assertEquals(income, new BigDecimal("5000.00"));
+    }
+
+    @Test
+    public void mustPay25PercentTaxOnIncomeLessThan20000And40PercentOnIncomeAbove20000() {
+        BigDecimal income = new BigDecimal("30000");
+        income = calculator.calculateTax(income);
+        assertEquals(income, new BigDecimal("9000.00"));
+    }
+
+    @Test
+    public void mustPay25PercentTaxOnIncomeLessThan20000And40PercentOnIncomeAbove20000_V1() {
+        BigDecimal income = new BigDecimal("21000");
+        income = calculator.calculateTax(income);
+        assertEquals(income, new BigDecimal("5400.00"));
+    }
+}
