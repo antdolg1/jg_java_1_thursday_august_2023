025ac449934d4f4919bb06d675254208ce2af3c8
ArtjomOssipov <ossipovartjom@gmail.com>
2024-01-04 15:42:47 +0200

    Artjom Ossipov, lesson 14, level 1|2

---
+++ src/main/java/students/artjom_ossipov/lesson_14/level_1_2/Apple.java
@@ -0,0 +1,42 @@
+package students.artjom_ossipov.lesson_14.level_1_2;
+
+import java.util.Objects;
+
+public class Apple {
+    private String color;
+    private int weight;
+
+    public Apple(String color, int weight) {
+        this.color = color;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Apple{" +
+                "color='" + color + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Apple apple = (Apple) o;
+        return weight == apple.weight && Objects.equals(color, apple.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color, weight);
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
